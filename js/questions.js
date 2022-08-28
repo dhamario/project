a43cd6820eb6ddbf8ccac71135a6ba2479311452
Dhamario Gymnastiarno <dhamario.sch@gmail.com>
2022-08-28 17:01:52 +0700

    push

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// Membuat Array dan Membuat Soal
+let questions = [
+  {
+  numb: 1,
+  question: "Apa singkatan dari HTML?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "Apa singkatan dari CSS?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 3,
+  question: "Apa singkatan dari PHP?",
+  answer: "Hypertext Preprocessor",
+  options: [
+    "Hypertext Preprocessor",
+    "Hypertext Programming",
+    "Hypertext Preprogramming",
+    "Hometext Preprocessor"
+  ]
+},
+  {
+  numb: 4,
+  question: "Apa singkatan dari SQL?",
+  answer: "Structured Query Language",
+  options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+  ]
+},
+  {
+  numb: 5,
+  question: "Apa singkatan dari XML?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+},
+];
